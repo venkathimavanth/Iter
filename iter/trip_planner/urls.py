5962bb4f3640a5fc1bc7a02fa4c2d1ed0841c063
hima <hima@hima.com>
2019-07-06 20:44:18 +0530

    7/6 9

--- iter/trip_planner/urls.py
+++ iter/trip_planner/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
 
+    path('planer-city=/(?P<value>\d+)/',views.trip_city,name='trip_city'),
     path('planer/',views.trip_plan,name='trip_plan'),
+    path('home/',views.trip_home,name='trip_home'),
 
     ]
